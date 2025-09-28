15fd41becc2c56ad2c63a575baa419ed3b87e04d
DavidVillanueva2 <David.Villanueva1@alu.uclm.es>
2025-09-28 20:04:48 +0200

    Update SequentialFile.java

--- src/SequentialFile.java
+++ src/SequentialFile.java
@@ -1,26 +1,40 @@
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- @param <T> 
-*/
+/**********
+ * 
+ * Class' name: SequentialFile
+ * Author's name: David
+ * Creation date: 28/9/2025
+ * Class version: First version.
+ * Class description: This class manages the file and gives manageable Strings to the class Satelite.
+ * 
+ * 
+ ***********/
 class SequentialFile<T extends DefineTipeT> {
     private FileReader reader;
     private int currentChar;
-    private final String separator;
 
-    /**
-     * @param fileName  
-     * @param separator 
-     * @throws IOException 
+    /********
+     * 
+     * Method's name: SequentialFile.
+     * Name of the original author: David
+     * Description on the method:  constructor method
+     * Calling arguments: 
+     * Return value: there is not a return value.
+     * 
      */
-    public SequentialFile(String fileName, String separator) throws IOException {
+    public SequentialFile(String fileName) throws IOException {
         this.reader = new FileReader(fileName);
-        this.separator = separator;
     }
 
-    /**
-     * @throws IOException 
+   /********
+     * 
+     * Method's name: close
+     * Name of the original author: David
+     * Description on the method: This method closes the file.
+     * Calling arguments: 
+     * 
      */
     public void close() throws IOException {
         if (reader != null) { // to avoid NullPointerException
@@ -28,12 +42,17 @@ class SequentialFile<T extends DefineTipeT> {
         }
     }
 
-    /**
-     * @return
-     * @throws IOException
+    /********
+     * 
+     * Method's name: ReadLine
+     * Name of the original author: David
+     * Description on the method: This method selects a line of data from all the file (in a sequential way).
+     * Calling arguments: 
+     * Return value: a String with a line of information of the file.
+     * 
      */
     public String readLine() throws IOException {
-        StringBuilder sb = new StringBuilder(); // special type of String that can be easily modified
+        StringBuilder sb = new StringBuilder(); // special type of String (not acurrate definition) that can be easily modified
         while ((currentChar = reader.read()) != -1) {
             if (currentChar == '\n') {
                 break; // I force the while loop to finish
@@ -48,9 +67,14 @@ class SequentialFile<T extends DefineTipeT> {
         return sb.toString(); // I create a string
     }
 
-    /**
-     * @param obj 
-     * @throws IOException 
+    /********
+     * 
+     * Method's name: ReadInto
+     * Name of the original author: David
+     * Description on the method: This method sends the line of the file to the method readData and saves it on the object.
+     * Calling arguments: a general tipe object.
+     * Return value: there is not a return value.
+     * 
      */
     public void readInto(T obj) throws IOException {
         String line = readLine();
@@ -59,8 +83,14 @@ class SequentialFile<T extends DefineTipeT> {
         }
     }
 
-    /**
-     * @throws IOException 
+    /********
+     * 
+     * Method's name: skipLine
+     * Name of the original author: David
+     * Description on the method: This method skips a line from the file
+     * Calling arguments: 
+     * Return value: there is not a return value.
+     * 
      */
     public void skipLine() throws IOException {
         while ((currentChar = reader.read()) != -1) {
@@ -70,24 +100,24 @@ class SequentialFile<T extends DefineTipeT> {
         }
     }
 
-    /**
-     * @return 
-     * @throws IOException 
+    /********
+     * 
+     * Method's name: endOfFile
+     * Name of the original author: David
+     * Description on the method: This method peaks if the file is finished or not.
+     * Calling arguments: 
+     * Return value: Boolean, true if the file has ended, and false if not
+     * 
      */
     public boolean endOfFile() throws IOException {
         reader.mark(1);
-        int c = reader.read();
-        if (c == -1) {
+        int a = reader.read();
+        if (a == -1) {
             return true;
         }
-        reader.reset(); //retrocede el marcador de lectura a su posicion antes de ser leido y guardado en c
+        reader.reset(); // sends the pointer of the lecture back to its original position.
         return false;
     }
 
-    /**
-     * @return The separator as a String
-     */
-    public String getSeparator() {
-        return separator;
-    }
+    
 }
